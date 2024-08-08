e611625429f2d1c8971d3d627d04dbab1066a856
sheik-mostafizur <sheikmostafizur@gmail.com>
2024-08-08 19:17:47 +0600

    files and directories can see base on path

---
+++ src/get-files.ts
@@ -0,0 +1,38 @@
+import { promises as fs } from "fs";
+import { Stats } from "fs";
+
+// Define an interface for file information
+interface FileInfo {
+  name: string;
+  stat: Stats;
+}
+
+export default async (dir_and_files: string[]): Promise<FileInfo[]> => {
+  // Process each file to get its stats and return as objects
+  const directoriesWithName = await Promise.all(
+    dir_and_files.map(async (file: string) => {
+      try {
+        const stat = await fs.stat(file);
+        // Include the file name and stat in the result
+        const result: FileInfo = { name: file, stat };
+        // Return only if it's a directory
+        if (stat.isFile()) {
+          return result;
+        }
+      } catch (error) {
+        console.error(`Error getting stats for file ${file}:`, error);
+        return undefined;
+      }
+    })
+  );
+
+  // Filter out undefined results (in case of errors or non-directory files)
+  const filteredDir = directoriesWithName.filter(
+    (file): file is FileInfo => file !== undefined
+  );
+
+  // Sort the directories by name
+  const sortDir = filteredDir.sort((a, b) => a.name.localeCompare(b.name));
+
+  return sortDir;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
+import { promises as fs } from "fs";
 import * as path from "path";
+import getDirectories from "./get-directories";
+import getFiles from "./get-files";
 
 const root = path.join(__dirname, "../");
-console.log(root);
+
+const generateFilesTree = async (location: string) => {
+  try {
+    const dir_and_files = await fs.readdir(location);
+
+    const directories = await getDirectories(dir_and_files);
+    const files = await getFiles(dir_and_files);
+
+    const daf = [...directories, ...files];
+
+    const dirAndFiles = daf.map((item) => item.name);
+
+    return dirAndFiles;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const main = async () => {
+  generateFilesTree(root)
+    .then((res) => console.log(res))
+    .catch((error) => console.error(error));
+};
+
+main();
